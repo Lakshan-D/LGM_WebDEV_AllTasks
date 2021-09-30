5dd7cbf6b9fddd588d25efcd674f3d806e898e0e
Lakshan-D <89792376+Lakshan-D@users.noreply.github.com>
2021-09-30 19:15:53 +0530

    Add files via upload

---
+++ ass8a.js
@@ -0,0 +1,17 @@
+function operators()
+{
+    alert("operator ans here")
+    document.write("ans here:")
+    var n1=21;
+    var n2=8;
+    add= n1+n2;
+    document.writeln(add); 
+    sub= n1-n2;
+    document.writeln(sub);
+    mul= n1*n2;
+    document.writeln(mul);
+    did= n1/n2;
+    document.writeln(did);
+    
+
+}
